8364352c08e6eae9de9c8921c5e244e2047f2917
chuanbinwu <wuchuanbin3013@163.com>
2016-01-06 23:19:31 +0800

    添加了数据入库的公共文件 ＃ config.php 配置文件 ＃test.php eg

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+	//引入操作类 一般不需要修改了
+	include_once('./BaseSql.class.php');
+
+	//以下是数据库的配置信息 轻根据本地状况配置
+
+	$config['host_name'] = '127.0.0.1';
+
+	$config['user'] = 'root';
+
+	$config['password'] = '123456';
+
+	$config['dbname'] = 'joke';
+
+	//实例化数据库操作类
+	$db = new BaseSql($config);
